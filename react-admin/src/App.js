45ceceb052c87677b8a74228eb4e25b7f53b7dc4
Clemente Nogueira <git.3gear@gmail.com>
2022-06-16 14:53:07 +0100

    The folder hierarchy of the src folder has been created. The data folder with images and application test file was created.

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './App.css';
 
 const App = () => {
     return (
-        <div>
+        <h1 className={'text-2xl font-bold'}>
             App
-        </div>
+        </h1>
     );
 };
 
