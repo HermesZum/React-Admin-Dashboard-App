4d4bde96b3caccbd2c7717a1271995cec5521169
Clemente Nogueira <git.3gear@gmail.com>
2022-06-16 15:02:46 +0100

    App.js file imports have been added.

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from 'react-icons/fi';
+import { Tooltip } from "@syncfusion/ej2-react-popups";
+
 import './App.css';
 
 const App = () => {
     return (
-        <h1 className={'text-2xl font-bold'}>
+        <h1 className={ 'text-2xl font-bold' }>
             App
         </h1>
     );
